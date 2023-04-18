a689693653b4e8b3979a7a8672856aa4c3e94592
rizwan2106 <rizwanfadillah62@gmail.com>
2023-04-19 00:40:46 +0700

    Halaman Admin Users

--- resources/views/user-banned.blade.php
+++ resources/views/user-banned.blade.php
@@ -1,14 +1,9 @@
 @extends('layouts.mainlayout')
 
-@section('title', 'Users')
+@section('title', 'Banned Users')
 
 @section('content')
-    <h1>User List</h1>
-
-    <div class="mt-5 d-flex justify-content-end">
-        <a href="/user-banned" class="btn btn-secondary me-3">View Banned User</a>
-        <a href="/registered-users" class="btn btn-primary">New Registered User</a>
-    </div>
+    <h1>Banned User List</h1>
 
     <div class="mt-5">
         @if (session('status'))
@@ -29,7 +24,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($users as $item)
+                @foreach ($bannedUsers as $item)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $item->username }}</td>
@@ -41,8 +36,7 @@
                             @endif
                         </td>
                         <td>
-                            <a href="/user-detail/{{$item->slug}}">Detail</a>
-                            <a href="/user-ban/{{$item->slug}}">Ban User</a>
+                            <a href="/user-restore/{{$item->slug}}">Restore</a>
                         </td>
                     </tr>
                 @endforeach
